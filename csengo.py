1b6023b0ece0762d730c1332e8d19299f6f7d941
Horák Attila <attila.horak.90@gmail.com>
2026-03-10 09:26:56 +0100

    first schedule

---
+++ csengo.py
@@ -0,0 +1,29 @@
+import time
+import schedule
+import vlc
+
+from lib import csengo_json
+
+def schedule_actions():
+
+  with open("csengo_config.json", "r") as file:
+    tartalom = file.read()
+    konfiguracio = csengo_json.CsengoKonfiguracio.model_validate_json(tartalom)
+
+  for idopont in konfiguracio.idopontok:
+    schedule.every().day.at(idopont.idopont).do(job, variable=idopont.zene)
+
+  while True:
+    schedule.run_pending()
+    time.sleep(5)
+
+def job(variable):
+    print(f"Playing: {variable}")
+    player = vlc.MediaPlayer(variable)
+    player.play()
+    time.sleep(10)
+    player.stop()
+    return
+
+print("Scheduler is starting...")
+schedule_actions()
